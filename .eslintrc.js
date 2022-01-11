88bf9e0b194ff87e6c877c513bde748ca076bb6b
hyo-choi <hyochoi31@gmail.com>
2022-01-11 20:32:36 +0900

    :wrench: Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,42 +2,42 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
-    }
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "plugin:storybook/recommended"],
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/jsx-filename-extension': 0,
     'react/function-component-definition': [2, {
-      namedComponents: 'arrow-function'
+      namedComponents: 'arrow-function',
     }],
     'import/extensions': ['error', {
-      ts: 'never'
+      ts: 'never',
     }],
     indent: 'off',
-    '@typescript-eslint/indent': ['error', 2]
+    '@typescript-eslint/indent': ['error', 2],
   },
   overrides: [{
     files: ['*.stories.tsx'],
     rules: {
       'react/jsx-props-no-spreading': 0,
       'react/function-component-definition': [0, {
-        namedComponents: 'arrow-function'
-      }]
-    }
-  }]
+        namedComponents: 'arrow-function',
+      }],
+    },
+  }],
 };
