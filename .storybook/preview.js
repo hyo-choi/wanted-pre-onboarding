63ec4e00b64197247cd58c133602a322c8d89aa7
hyo-choi <hyochoi31@gmail.com>
2022-01-11 20:29:09 +0900

    :lipstick: Storybook viewport 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,38 @@
+import 'minireset.css';
+import 'normalize.css';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  'max': {
+    name: 'larger than 1100px',
+    styles: {
+      width: '1200px',
+      height: 'auto',
+    }
+  },
+  '1100': {
+    name: 'less than 1100px',
+    styles: {
+      width: '1100px',
+      height: 'auto',
+    }
+  },
+  '991': {
+    name: 'less than 991px',
+    styles: {
+      width: '991px',
+      height: 'auto',
+    }
+  },
+  'min': {
+    name: 'less than 767px',
+    styles: {
+      width: '767px',
+      height: 'auto',
+    }
+  }
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +41,10 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...customViewports,
+    },
+  },
+};
