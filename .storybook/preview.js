c53edfe5e81ca7fded306d203d8423c169436bda
hyo-choi <hyochoi31@gmail.com>
2022-01-12 20:49:50 +0900

    :lipstick: viewport height 조정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,35 +7,35 @@ const customViewports = {
     name: 'larger than 1200px',
     styles: {
       width: '1300px',
-      height: 'auto',
+      height: '80vh',
     }
   },
   '1199': {
     name: 'less than 1199px',
     styles: {
       width: '1198px',
-      height: 'auto',
+      height: '80vh',
     },
   },
   '1100': {
     name: 'less than 1100px',
     styles: {
       width: '1099px',
-      height: 'auto',
+      height: '80vh',
     }
   },
   '991': {
     name: 'less than 991px',
     styles: {
       width: '990px',
-      height: 'auto',
+      height: '80vh',
     }
   },
   'min': {
     name: 'less than 767px',
     styles: {
       width: '766px',
-      height: 'auto',
+      height: '80vh',
     }
   }
 };
